03cdf31446c3f0cfef5bfdfd37ce1329b5406cd0
Fawwaaz777 <118934189+Fawwaaz777@users.noreply.github.com>
2023-04-20 22:43:31 +0400

    Add addition operation

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var randomNumber = new Random(1);
+int firstNumber = randomNumber.Next();
+int secondNumber = randomNumber.Next();
+
+Console.WriteLine(firstNumber +" + "+ secondNumber);
+int answer;
+answer = Convert.ToInt32(Console.ReadLine());
+if((firstNumber+secondNumber)== answer){
+    Console.WriteLine("Correct");
+}
+else{
+    Console.WriteLine("Wrong");
+}
